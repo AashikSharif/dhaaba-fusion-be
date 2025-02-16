3a08befb3d2af2c889c3c79808d77ec4e5d3ab80
Aashik Sharif B <ashiktcy.s@gmail.com>
2025-02-15 17:36:29 -0800

    POST request for login and registration has been set

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
 const app = express();
+connectDB();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json()); // Body parser
+app.use(cors()); // Allow CORS
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-});
+// Routes
+app.use('/api/auth', require('./routes/auth'));
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
